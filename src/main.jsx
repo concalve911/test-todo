beafa38f6cefb53c5c09bf32d5d02b5e937b4162
concalve911 <aleksandrgarkusha911@gmail.com>
2024-11-10 17:55:19 +0100

    third

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from "./App";
 import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>
-  </HashRouter>
+    </HashRouter>
+  </React.StrictMode>
 );
